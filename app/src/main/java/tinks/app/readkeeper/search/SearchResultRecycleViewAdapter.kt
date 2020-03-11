c48f08e7d7ba4d296f9c28c0fd0276b65dd954fd
Tink Zhang <ZhangYunfengZJU@gmail.com>
2020-03-11 22:03:41 +0800

    [2] Add more data for Search Result items.

--- app/src/main/java/tinks/app/readkeeper/search/SearchResultRecycleViewAdapter.kt
+++ app/src/main/java/tinks/app/readkeeper/search/SearchResultRecycleViewAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import tinks.app.readkeeper.R
+import tinks.app.readkeeper.ReadKeeperApplication
 
 class SearchResultRecycleViewAdapter(var searchBooks: List<SearchBook>) :
     RecyclerView.Adapter<ViewHolder>() {
@@ -19,11 +20,22 @@ class SearchResultRecycleViewAdapter(var searchBooks: List<SearchBook>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.titleTextView?.text = searchBooks[position].title
+        val book = searchBooks[position]
+        val resource = ReadKeeperApplication.resourses
+        holder.titleTextView?.text = book.title
+        holder.authorTextView?.text = resource
+            .getString(R.string.author_by, book.author)
+        holder.publishTextView?.text = resource
+            .getString(R.string.publish_in, book.originalPublicationYear)
+        holder.ratingTextView?.text = resource
+            .getString(R.string.rating, book.averageRating, book.ratingsCount)
     }
 
 }
 
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var titleTextView: TextView? = itemView.findViewById(R.id.title_textview)
+    var authorTextView: TextView? = itemView.findViewById(R.id.author_textview)
+    var publishTextView: TextView? = itemView.findViewById(R.id.publish_time_textview)
+    var ratingTextView: TextView? = itemView.findViewById(R.id.rating_textview)
 }
