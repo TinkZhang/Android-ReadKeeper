78808c86e3e423e2b9983ec72e51c1f03165cc0d
Tink Zhang <ZhangYunfengZJU@gmail.com>
2020-03-12 11:37:46 +0800

    [2] Add Image for search book item

--- app/src/main/java/tinks/app/readkeeper/AppConfig.kt
+++ app/src/main/java/tinks/app/readkeeper/AppConfig.kt
@@ -1,6 +1,9 @@
 package tinks.app.readkeeper
 
 object AppConfig {
+    val DEFAULT_GOODREADER_IMAGE =
+        "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png"
+
     object Search {
         val MAX_SEARCH_RESULT = 20
     }
